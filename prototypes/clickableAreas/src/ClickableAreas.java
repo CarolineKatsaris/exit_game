4c471f9dfca9000e6b11eb8ef11b6f52197810fa
sandrasowa <sandrasowa@gmx.de>
2025-10-15 19:30:11 +0200

    Test

--- prototypes/clickableAreas/src/ClickableAreas.java
+++ prototypes/clickableAreas/src/ClickableAreas.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
 public class ClickableAreas {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // ich kommentiere zu Testzwecken
         SwingUtilities.invokeLater(() -> {
             ClickableAreasUI gameUI = new ClickableAreasUI();
             gameUI.setVisible(true);
