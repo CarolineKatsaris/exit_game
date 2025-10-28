7d8585da6c56d12daa41e14127c6a72b42c391f0
Sandra Sowa <go36fop@mytum.de>
2025-10-28 16:38:01 +0100

    Starttext (nicht dynamisch, da hier noch keine Benutzeranrede) hinzugefügt und...

--- main/src/view/StartView.java
+++ main/src/view/StartView.java
@@ -19,6 +19,28 @@ public class StartView extends JPanel {
         ImageIcon img = new ImageIcon(imgUrl);
         JLabel imgLabel = new JLabel(img);
 
+        // Starttext (ohne Benutzeransprache, da vor dem Login)
+        imgLabel.setLayout(new BorderLayout()); //neuer "Container" für den Text, erstmal als eher unflexibles
+                                                // BorderLayout, kann man leicht abändern
+
+        JLabel introText = new JLabel("Willkommen im digitalen Escape Room!");
+        introText.setHorizontalAlignment(SwingConstants.CENTER);  // Zentrieren (oder ggf anders)
+        introText.setFont(new Font("SansSerif", Font.BOLD, 20));   // Schriftart und Größe
+        introText.setForeground(Color.WHITE);
+
+        JLabel subText = new JLabel("Finde Hinweise, löse Rätsel und stoppe den Virus!");
+        subText.setHorizontalAlignment(SwingConstants.CENTER); // Zentrieren
+        subText.setFont(new Font("SansSerif", Font.PLAIN, 16));
+        subText.setForeground(Color.WHITE);
+
+        JPanel textPanel = new JPanel(new GridLayout(2, 1)); //Legt den Text in zwei Zeilen und eine Spalte
+        textPanel.setOpaque(false); // durchsichtig, damit das Bild sichtbar bleibt (Opaque ist erstmal nicht transparent)
+        textPanel.add(introText);
+        textPanel.add(subText);
+
+        imgLabel.add(textPanel, BorderLayout.CENTER); // legt Text auf das Bild
+
+
         startButton = new JButton("Virus stoppen");
 
         JPanel bottom = new JPanel();
