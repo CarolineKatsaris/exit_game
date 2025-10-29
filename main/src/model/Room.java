f9162f6a6e50eebea2245e6c98aefb823300918a
Svenja Dorsch <go36fal@mytum.de>
2025-10-29 12:50:03 +0100

    Svenjas branch

--- main/src/model/Room.java
+++ main/src/model/Room.java
@@ -1,14 +1,41 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
 
     private final String roomTitle;
-
     private boolean open;
+    private boolean completed;
+    private List<Quiz> quizzes = new ArrayList<>();
+    private int currentQuizIndex = 0;
 
     public Room(String roomTitle, boolean open) {
         this.roomTitle = roomTitle;
         this.open = open;
+    }
+
+    boolean isCompleted() {
+        //Raum ist abgeschlossen, wenn alle Quizzes abgeschlossen sind
+        for (Quiz quiz:quizzes){
+            if(!quiz.isCompleted()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //ersetzt den Setter, da hier der abgeschlossene Raum markiert wird
+    //es gibt ja in der Regel kein Zurück von abgeschlossen
+    void markAsCompleted() {
+        this.completed = true;
+    }
+
+    //sollte der Raum bei einem Spielneustart doch zurückgesetzt werden müssen, dann über diese Methode
+    void resetRoom() {
+        this.open = false;
+        this.completed = false;
     }
 
     public String getRoomTitle() {
@@ -23,9 +50,24 @@ public class Room {
         this.open = open;
     }
 
-
-
-
-    //Liste der Rätsel
+    //Beantwortet, welches Quiz gerade aktiv ist
+    Quiz getCurrentQuiz() {
+        return quizzes.get(currentQuizIndex);
     }
+
+    void setCurrentQuizIndex(int index) {
+        if (index >= 0 && index < quizzes.size()) {
+            this.currentQuizIndex = index;
+        }
+    }
+
+    void setQuizzes(List<Quiz> quizzes) {
+        this.quizzes = new ArrayList<>(quizzes);
+    }
+
+    //von hier bekommt man die Liste der im Raum vorhandenen Quizzes
+    List<Quiz>getQuizzes(){
+        return new ArrayList<>(quizzes);
+    }
+}
 
